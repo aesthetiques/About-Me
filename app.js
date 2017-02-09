93fc3bbc34cc2bca4613c07ee5e8de87a4fde73d
Caleb Wells <caleb.wells1@gmail.com>
2017-02-08 19:01:48 -0800

    near finished, adding css + added name personalization

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 var confirmQuiz = confirm('Hello, would you like to play a game?');
 
 if(confirmQuiz === true){
-  console.log('Good... Welcome to the Monkey House.');
+  console.log('Good... Welcome to the Monkey House.. What would you like to be called?');
+  var name = prompt('Good... Welcome to the Monkey House.. What would you like to be called?');
 } else {
-  console.log('Ohohoho, its much too late to be shy.. Come into the Monkey House.');
+  console.log('Ohohoho, its much too late to be shy.. Come into the Monkey House... What do we call you?');
+  var name = prompt('Ohohoho, its much too late to be shy.. Come into the Monkey House... What do we call you?');
 }
 
 var quizResponseOne = prompt('Please answer with Y or N. Does Caleb have a dog?').toUpperCase();
@@ -15,11 +17,11 @@ console.log(quizResponseOne);
 if(quizResponseOne === 'Y' || quizResponseOne === 'YES' ){
   console.log('You\'re correct!');
   document.write('<h4>Please answer with Y or N. Does Caleb have a dog?</h4>');
-  document.write('<h5>You said: ' + quizResponseOne + '. <strong>You\'re correct!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseOne + '. <strong>You\'re correct, ' + name + '!</strong></h5>');
 } else if(quizResponseOne === 'N' || quizResponseOne === 'NO'){
   console.log('You\'re incorrect, try again');
   document.write('<h4>Please answer with Y or N. Does Caleb have a dog?</h4>');
-  document.write('<h5>You said: ' + quizResponseTwo + '. <strong>You\'re INcorrect!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseOne + '. <strong>You\'re INcorrect, ' + name + '!</strong></h5>');
 } else{
   alert('That\'s not an answer, reload and try again.');
 }
@@ -29,11 +31,11 @@ var quizResponseTwo = prompt('You know the drill. Does Caleb have any tattoos?')
 if(quizResponseTwo === 'Y' || quizResponseTwo === 'YES'){
   console.log('You\'re correct!');
   document.write('<h4>You know the drill. Does Caleb have tattoos?</h4>');
-  document.write('<h5>You said: ' + quizResponseOne + '. <strong>You\'re correct!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseTwo + '. <strong>You\'re INcorrect, ' + name + '!</strong></h5>');
 } else if(quizResponseTwo === 'N' || quizResponseTwo === 'NO'){
   console.log('You\'re incorrect, try again');
   document.write('<h4>You know the drill. Does Caleb have tattoos?</h4>');
-  document.write('<h5>You said: ' + quizResponseTwo + '. <strong>You\'re INcorrect!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseTwo + '. <strong>You\'re INcorrect, ' + name + '!</strong></h5>');
 } else{
   alert('That\'s not an answer, reload and try again.');
 }
@@ -43,11 +45,11 @@ var quizResponseThree = prompt('Does Caleb have a cat? Y or N.').toUpperCase();
 if(quizResponseThree === 'Y' || quizResponseThree === 'YES'){
   console.log('You\'re correct!');
   document.write('<h4>Does Caleb have a cat? Y or N.</h4>');
-  document.write('<h5>You said: ' + quizResponseOne + '. <strong>You\'re correct!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseThree + '. <strong>You\'re correct, ' + name + '!</strong></h5>');
 } else if(quizResponseThree === 'N' || quizResponseThree === 'NO'){
   console.log('You\'re incorrect, try again');
   document.write('<h4>Does Caleb have cats? Y or N.</h4>');
-  document.write('<h5>You said: ' + quizResponseTwo + '. <strong>You\'re INcorrect!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseThree + '. <strong>You\'re INcorrect, ' + name + '!</strong></h5>');
 } else{
   alert('That\'s not an answer, reload and try again.');
 }
@@ -57,11 +59,11 @@ var quizResponseFour = prompt('Is Caleb\'s PC better than yours?').toUpperCase()
 if(quizResponseFour === 'Y' || quizResponseFour === 'YES'){
   console.log('You\'re correct!');
   document.write('<h4>Is Caleb\'s PC better than yours?</h4>');
-  document.write('<h5>You said: ' + quizResponseOne + '. <strong>You\'re correct!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseFour + '. <strong>You\'re correct, ' + name + '!</strong></h5>');
 } else if(quizResponseFour === 'N' || quizResponseFour === 'NO'){
   console.log('You\'re incorrect, try again');
   document.write('<h4>Is Caleb\'s PC better than yours?</h4>');
-  document.write('<h5>You said: ' + quizResponseFour + '. <strong>You\'re INcorrect!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseFour + '. <strong>You\'re INcorrect, ' + name + '!</strong></h5>');
 } else{
   alert('That\'s not an answer, reload and try again.');
 }
@@ -71,11 +73,11 @@ var quizResponseFive = prompt('Would Caleb ever use an iPhone?').toUpperCase();
 if(quizResponseFive === 'Y' || quizResponseFive === 'YES'){
   console.log('You\'re correct!');
   document.write('<h4>Would Caleb ever use an iPhone?</h4>');
-  document.write('<h5>You said: ' + quizResponseOne + '. <strong>You\'re correct!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseFive + '. <strong>You\'re correct, ' + name + '!</strong></h5>');
 } else if(quizResponseFive === 'N' || quizResponseFive === 'NO'){
   console.log('You\'re incorrect, try again');
   document.write('<h4>Would Caleb ever use an iPhone?</h4>');
-  document.write('<h5>You said: ' + quizResponseFive + '. <strong>You\'re INcorrect!</strong></h5>');
+  document.write('<h5>You said: ' + quizResponseFive + '. <strong>You\'re INcorrect, ' + name + '!</strong></h5>');
 } else{
   alert('That\'s not an answer, reload and try again.');
 }
